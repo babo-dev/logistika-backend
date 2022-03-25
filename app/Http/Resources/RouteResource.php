801b8d3611d050516b2c9f81de8203700071949d
Babanyyaz <camanow259@gmail.com>
2022-03-25 17:52:12 +0500

    home route routes that are not up today

--- app/Http/Resources/RouteResource.php
+++ app/Http/Resources/RouteResource.php
@@ -3,7 +3,6 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use Nette\Utils\DateTime;
 
 class RouteResource extends JsonResource
 {
@@ -21,14 +20,11 @@ class RouteResource extends JsonResource
            'id' => $this->id,
            'date1' => $this->date1,
            'date2' => $this->date2,
-           // 'date1' => date_format( new DateTime($this->date1), "d.m.Y"),
-           // 'date2' => date_format( new DateTime($this->date2), "d.m.Y"),
            'car_body' => $this->car_body, 
            'company' => new CompanyResource($this->company), 
            'note' => $this->note,
            'source' => $this->source,
            'destination' => $this->destination,
-           /* 'destination' => $this->state->title, */
         ];
     }
 }
