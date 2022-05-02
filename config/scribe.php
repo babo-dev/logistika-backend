a979d3fc94b2f560cb8bcc967d80b1b4a808e927
Babanyyaz <camanow259@gmail.com>
2022-05-02 16:10:27 +0500

    api doc v0.1

--- config/scribe.php
+++ config/scribe.php
@@ -19,7 +19,7 @@ return [
     /*
      * The base URL displayed in the docs. If this is empty, Scribe will use the value of config('app.url').
      */
-    'base_url' => null,
+    'base_url' => '127.0.0.1:8080',
 
     /*
      * Tell Scribe what routes to generate documentation for.
@@ -255,11 +255,9 @@ return [
      */
     'intro_text' => <<<INTRO
 This documentation aims to provide all the information you need to work with our API.
-
 <aside>As you scroll, you'll see code examples for working with the API in different programming languages in the dark area to the right (or as part of the content on mobile).
 You can switch the language used with the tabs at the top right (or from the nav menu at the top left on mobile).</aside>
-INTRO
-    ,
+INTRO,
 
     /*
      * Example requests for each endpoint will be shown in each of these languages.
