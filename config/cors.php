1a3b2f0a6260754e79669471d71a994a39a39da2
Babanyyaz <camanow259@gmail.com>
2022-05-14 18:18:58 +0500

    cors updated

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'https://iber.biz', 'http://iber.biz'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['/https?:\/\/iber\.iber\/?\z/'],
 
     'allowed_headers' => ['*'],
 
