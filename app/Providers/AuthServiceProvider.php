07fcf6a7ac194a2b00a16b5182a7ed800aa39c1d
Babanyyaz <camanow259@gmail.com>
2022-05-26 18:00:32 +0500

    reset password link

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Auth\Notifications\ResetPassword;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -33,6 +34,10 @@ class AuthServiceProvider extends ServiceProvider
         ->subject('Verify Email Address')
         ->line('Click the button below to verify your email address.')
         ->action('Verify Email Address', $spaUrl);
+    });
+
+    ResetPassword::createUrlUsing(function ($user, string $token) {
+      return 'https://iber.biz/reset?token=' . $token . '&' . 'email=' . $user->email;
     });
   }
 }
