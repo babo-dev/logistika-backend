031e82a633413b4d1223aea157c1d43e6b37a348
Babanyyaz <camanow259@gmail.com>
2022-06-08 14:24:21 +0500

    new data after verification

--- app/Http/Controllers/EmailVerificationController.php
+++ app/Http/Controllers/EmailVerificationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CompanyResource;
+use App\Http\Resources\UserResource;
 use App\Models\Company;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -64,7 +66,12 @@ class EmailVerificationController extends Controller
     return response()->json([
       'success' => 'true',
       'data' => [
-        'access_token' => auth()->login($user)
+        'access_token' => auth()->login($user),
+        'token_type' => 'bearer',
+        'expires_in' => auth()->factory()->getTTL() * 60,
+        'data' => $type=='user' ? new UserResource($user) : new CompanyResource($user),
+        'account_type' => $type,
+        'mail_confirmed' => $user->email_verified_at ? 1 : 0
       ],
       'message' => 'Email has been verified'
     ]);
