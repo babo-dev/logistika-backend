b01b0076213d0046e1c8774715db6ecfbfe90673
Babanyyaz <camanow259@gmail.com>
2022-06-27 09:25:59 +0500

    company email error 1

--- app/Http/Controllers/EmailVerificationController.php
+++ app/Http/Controllers/EmailVerificationController.php
@@ -68,7 +68,6 @@ class EmailVerificationController extends Controller
         ->action($this->actionText, $spaUrl);
     });
     $request->user()->notify(new VerifyEmail);
-    return $request->user();
     // it's the same thing
     // $request->user()->sendEmailVerificationNotification('tm');
 
