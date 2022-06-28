64b55aaee62f19fef7008542e54e130fe58abc74
Babanyyaz <camanow259@gmail.com>
2022-06-28 14:08:27 +0500

    template is ready, create job

--- resources/views/mail/main.blade.php
+++ resources/views/mail/main.blade.php
@@ -25,14 +25,53 @@
       </div>
     </section>
 
-    <section></section>
+    <section class="section_2">
+      <div>
+        <p class="first">Haryt</p>
+        <p> <b> arduino set </b></p>
+      </div>
+      <div>
+        <p class="first">Agramy</p>
+        <p><b> - </b></p>
+      </div>
+      <div>
+        <p class="first">Kub</p>
+        <p><b> - </b></p>
+      </div>
+      <div>
+        <p class="first">Byudjet</p>
+        <p>-</p>
+      </div>
+      <div>
+        <p class="first">Awtoulag gornusi</p>
+        <p> <b> - </b> </p>
+      </div>
+      <div>
+        <p class="first">Belli</p>
+        <p> <b> - </b> </p>
+      </div>
+    </section>
 
-    <section></section>
+    <section class="section_3">
+      <div>
+        <p class="first">Musderi ady</p>
+        <p> <b> Transporter </b> </p>
+      </div>
+      <div>
+        <p class="first">E-mail</p>
+        <p> <b> ss.transporter99@gmail.com </b> </p>
+      </div>
+      <div>
+        <p class="first">Telefon</p>
+        <p> <b> +99363997091 </b> </p>
+      </div>
+    </section>
   </div>
 
   <style>
     body {
-      font-size: 18px;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+      font-size: 16px;
       color: #837e7e;
     }
 
@@ -45,10 +84,40 @@
     }
 
     .section_1 {
+      line-height: 1.5rem;
       border-bottom: 1px solid #d9d9d9;
       display: flex;
       justify-content: space-between;
       padding-bottom: 10px;
+    }
+
+    .section_2 {
+      border-bottom: 1px solid #d9d9d9;
+      display: flex;
+      flex-direction: column;
+      padding: 10px 0;
+    }
+
+    .section_2 div {
+      display: flex;
+    }
+
+    .section_2 div .first {
+      width: 14rem;
+    }
+
+    .section_3 {
+      display: flex;
+      flex-direction: column;
+      padding: 10px 0;
+    }
+
+    .section_3 div {
+      display: flex;
+    }
+
+    .section_3 div .first {
+      width: 14rem;
     }
   </style>
 </body>
