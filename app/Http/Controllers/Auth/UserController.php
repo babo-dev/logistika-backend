3d8b6b17c656d59d005b95640aab2f856853e6b8
Babanyyaz <camanow259@gmail.com>
2022-07-06 19:31:04 +0500

    users and companies with unique emails

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -138,7 +138,7 @@ class UserController extends Controller
   {
     $validator = Validator::make($request->all(), [
       'name' => 'required|string|between:2,100',
-      'email' => 'required|string|email|max:100|unique:users,email',
+      'email' => 'required|string|email|max:100|unique:users,email|unique:companie,email',
       'password' => 'required|string|min:6',
       'phone' => 'required',
       'country' => 'required',
