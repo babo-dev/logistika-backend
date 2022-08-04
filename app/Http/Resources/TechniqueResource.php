e91a2556c2a0f100b698191a1bb54b2327bc8f58
Babanyyaz <camanow259@gmail.com>
2022-08-04 12:19:48 +0500

    technique resource use company resourde

--- app/Http/Resources/TechniqueResource.php
+++ app/Http/Resources/TechniqueResource.php
@@ -24,7 +24,8 @@ class TechniqueResource extends JsonResource
     /* return parent::toArray($request); */
     return [
       'id' => $this->id,
-      'company' => $this->company,
+      // 'company' => $this->company,
+      'company' => new CompanyResource($this->company),
       'owner' => $this->company->owner,
       'state' => $this->state,
       'technique_type' => $this->technique_type->title,
